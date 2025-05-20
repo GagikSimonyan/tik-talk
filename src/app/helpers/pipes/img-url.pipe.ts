9af10a2ae4f46ed66b114f7db79ce2b25c8563b0
Gagik Simonyan <gagik.simonyan92@gmail.com>
2025-05-20 22:36:27 +0400

    step 2 - made profile service, interface and imgUrl pipe, updated scss

---
+++ src/app/helpers/pipes/img-url.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'imgUrl'
+})
+export class ImgUrlPipe implements PipeTransform {
+
+  transform(value: string | null): string {
+    if (!value) {return ''}
+    return `https://icherniakov.ru/yt-course/${value}`;
+  }
+
+}
